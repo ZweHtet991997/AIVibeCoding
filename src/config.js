22607a263404b0ade437fe06f6d82ab4ef8335f9
unknown <kokonaung.kk53@gmail.com>
2025-08-07 10:54:23 +0630

    modify config

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const apiConfig = {
   // Set your backend API base URL here. You can use an environment variable or edit directly for now.
   baseUrl: process.env.REACT_APP_API_BASE_URL || 'https://9jrrt2wr-7177.inc1.devtunnels.ms',
   // Set your frontend base URL here. You can use an environment variable or edit directly for now.
-  frontendUrl: REACT_FRONTEND_URL,
+  frontendUrl: process.env.REACT_FRONTEND_URL,
 };
 
 export default apiConfig; 
